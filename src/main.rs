a0b59d3c0c4726b6a186d2f5f9025e5950ad3565
jackschu <js9pa@virginia.edu>
2023-09-09 13:24:51 -0400

    subtile selection is exfiltrated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use eframe::egui;
-use egui::{pos2, vec2, Color32, Pos2, Rect, Stroke, Ui};
+use egui::{pos2, vec2, Color32, Id, Pos2, Rect, Stroke, Ui};
 
 fn main() -> Result<(), eframe::Error> {
     //    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -19,6 +19,8 @@ struct MyApp {
     age: usize,
 }
 
+const SUBTILE_ID: &str = "subtile";
+
 impl Default for MyApp {
     fn default() -> Self {
         Self {
@@ -26,36 +28,6 @@ impl Default for MyApp {
             age: 80,
         }
     }
-}
-
-fn tile_ui(ui: &mut egui::Ui, size: f32, on: &mut bool) -> egui::Response {
-    let (rect, mut response) = ui.allocate_exact_size(vec2(size, size), egui::Sense::click());
-    if response.clicked() {
-        *on = !*on;
-        response.mark_changed();
-    }
-
-    if ui.is_rect_visible(rect) {
-        let visuals = ui.style().interact_selectable(&response, *on);
-        let rect = rect.expand(visuals.expansion);
-        ui.painter()
-            .rect(rect, 0.0, visuals.bg_fill, visuals.bg_stroke);
-    }
-
-    let center = rect.center();
-    let mini_size = size / 3.0;
-    let mini_rect = Rect::from_center_size(center, vec2(mini_size, mini_size));
-    let parent_id = ui.id();
-
-    let mini_response = ui.interact(mini_rect, parent_id.with("middle"), egui::Sense::click());
-    if ui.is_rect_visible(rect) {
-        let mini_visuals = ui.style().interact_selectable(&mini_response, *on);
-        let mini_rect = mini_rect.expand(mini_visuals.expansion);
-        ui.painter()
-            .rect(mini_rect, 0.0, mini_visuals.bg_fill, mini_visuals.bg_stroke);
-    }
-
-    return response;
 }
 
 pub fn board(ui: &mut Ui, columns: usize, rows: usize, roundness: f32, spacing: egui::Vec2) {
@@ -91,6 +63,87 @@ pub fn board(ui: &mut Ui, columns: usize, rows: usize, roundness: f32, spacing: 
     }
 }
 
+fn tile_ui(ui: &mut egui::Ui, size: f32, on: &mut bool) -> egui::Response {
+    let (rect, mut response) = ui.allocate_exact_size(vec2(size, size), egui::Sense::click());
+    if response.clicked() {
+        *on = !*on;
+        response.mark_changed();
+    }
+
+    if ui.is_rect_visible(rect) {
+        let visuals = ui.style().interact_selectable(&response, *on);
+        let rect = rect.expand(visuals.expansion);
+        ui.painter()
+            .rect(rect, 0.0, visuals.bg_fill, visuals.bg_stroke);
+    }
+
+    struct SquareDef {
+        pub dx: i8,
+        pub dy: i8,
+        pub id: String,
+    }
+    let square_defns = [
+        SquareDef {
+            dx: 0,
+            dy: 0,
+            id: "center".to_owned(),
+        },
+        SquareDef {
+            dx: -1,
+            dy: 0,
+            id: "left".to_owned(),
+        },
+        SquareDef {
+            dx: 1,
+            dy: 0,
+            id: "right".to_owned(),
+        },
+        SquareDef {
+            dx: 0,
+            dy: -1,
+            id: "top".to_owned(),
+        },
+        SquareDef {
+            dx: 0,
+            dy: 1,
+            id: "bottom".to_owned(),
+        },
+    ];
+
+    let center = rect.center();
+    let mini_size = size / 3.0;
+
+    for def in square_defns {
+        let mini_rect = Rect::from_center_size(
+            pos2(
+                center.x + (def.dx as f32) * mini_size,
+                center.y + (def.dy as f32) * mini_size,
+            ),
+            vec2(mini_size, mini_size),
+        );
+        let mini_response = rect_button(ui, mini_rect, ui.id().with(def.id.as_str()));
+        if mini_response.clicked() {
+            response.ctx.data_mut(|map| {
+                let id = Id::new(SUBTILE_ID);
+                map.insert_temp::<String>(id, def.id);
+            });
+        }
+    }
+
+    return response;
+}
+
+fn rect_button(ui: &mut egui::Ui, rect: Rect, id: Id) -> egui::Response {
+    let response = ui.interact(rect, id, egui::Sense::click());
+    let visuals = ui.style().interact(&response);
+    if ui.is_rect_visible(rect) {
+        let rect = rect.expand(visuals.expansion);
+        ui.painter()
+            .rect(rect, 0.0, visuals.bg_fill, visuals.bg_stroke);
+    }
+    response
+}
+
 pub fn tile(size: f32, on: &mut bool) -> impl egui::Widget + '_ {
     move |ui: &mut egui::Ui| tile_ui(ui, size, on)
 }
@@ -120,7 +173,17 @@ impl eframe::App for MyApp {
                 for r in 0..grid_rows {
                     for c in 0..grid_cols {
                         let mut on = false;
-                        ui.push_id((r, c), |ui| ui.add(tile(self.age as f32, &mut on)));
+                        let response = ui
+                            .push_id((r, c), |ui| ui.add(tile(self.age as f32, &mut on)))
+                            .inner;
+                        response.ctx.data_mut(|map| {
+                            let subtile_id = Id::new(SUBTILE_ID);
+                            let maybe_val = map.get_temp::<String>(subtile_id);
+                            if let Some(val) = maybe_val {
+                                println!("{:?}", val);
+                            }
+                            map.remove::<String>(subtile_id);
+                        })
                     }
                     ui.end_row();
                 }
