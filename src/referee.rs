1c615bf4102381b1cc2d08b444f32cf02ddc8f0d
jackschu <js9pa@virginia.edu>
2023-09-09 20:58:49 -0400

    rendering colors based on board data

--- src/referee.rs
+++ src/referee.rs
@@ -1,23 +1,26 @@
 use std::sync::mpsc::{Receiver, Sender};
 
+use egui::Color32;
+
 use crate::render::Message;
 
-const BOARD_SIZE: usize = 72 * 72;
+const BOARD_DIM: usize = 72;
+const BOARD_SIZE: usize = BOARD_DIM * BOARD_DIM;
 
 pub struct Board {
-    data: [Tile; BOARD_SIZE],
+    data: [Option<PlacedTile>; BOARD_SIZE],
 }
 
-#[derive(Copy, Clone)]
-enum Tile {
-    PlacedTile(PlacedTile),
-    EmptyTile,
+impl Board {
+    pub fn at(&self, row: usize, col: usize) -> &Option<PlacedTile> {
+        return &self.data[BOARD_DIM * col + row];
+    }
 }
 
 impl Default for Board {
     fn default() -> Self {
         Board {
-            data: [Tile::EmptyTile; BOARD_SIZE],
+            data: [None; BOARD_SIZE],
         }
     }
 }
@@ -25,19 +28,43 @@ impl Default for Board {
 #[derive(Copy, Clone)]
 pub struct PlacedTile {
     pub has_emblem: bool,
-    data: [MiniTile; 5],
+    // [top, left, center, right, bottom]
+    pub data: [MiniTile; 5],
 }
 
 #[derive(Copy, Clone)]
-enum MiniTile {
+pub enum MiniTile {
     Grass,
     Road,
     City,
     Monastery,
+    Junction,
+}
+
+impl MiniTile {
+    pub fn getColor(&self) -> Color32 {
+        match self {
+            Self::Grass => Color32::GREEN,
+            Self::Road => Color32::WHITE,
+            Self::City => Color32::BROWN,
+            Self::Monastery => Color32::RED,
+            Self::Junction => Color32::YELLOW,
+        }
+    }
 }
 
 pub fn referee_main(receiver: Receiver<Message>, sender: Sender<Board>) {
-    let board = Board::default();
+    let mut board = Board::default();
+    board.data[0] = Some(PlacedTile {
+        has_emblem: false,
+        data: [
+            MiniTile::Grass,
+            MiniTile::Grass,
+            MiniTile::Grass,
+            MiniTile::Grass,
+            MiniTile::Grass,
+        ],
+    });
     sender.send(board).unwrap();
     loop {
         match receiver.recv().unwrap() {
--- src/render.rs
+++ src/render.rs
@@ -1,9 +1,9 @@
 use std::sync::mpsc::{Receiver, Sender};
 
 use eframe::egui;
-use egui::{pos2, vec2, Id, Rect};
+use egui::{pos2, vec2, Color32, Id, Rect};
 
-use crate::referee::Board;
+use crate::referee::{Board, PlacedTile};
 
 pub enum Message {
     PrintMessage(String),
@@ -29,49 +29,60 @@ impl MyApp {
     }
 }
 
-fn tile_ui(ui: &mut egui::Ui, size: f32, on: &mut bool) -> egui::Response {
-    let (rect, mut response) = ui.allocate_exact_size(vec2(size, size), egui::Sense::click());
-    if response.clicked() {
-        *on = !*on;
-        response.mark_changed();
-    }
+fn tile_ui(ui: &mut egui::Ui, size: f32, tile: &Option<PlacedTile>) -> egui::Response {
+    let (rect, response) = ui.allocate_exact_size(vec2(size, size), egui::Sense::click());
+
+    let default_color = Color32::GRAY;
 
     if ui.is_rect_visible(rect) {
-        let visuals = ui.style().interact_selectable(&response, *on);
+        let visuals = ui.style().interact(&response);
         ui.painter()
-            .rect(rect, 0.0, visuals.bg_fill, visuals.bg_stroke);
+            .rect(rect, 0.0, default_color, visuals.bg_stroke);
     }
+
+    let get_color = |location: usize| {
+        if let Some(place_tile) = tile {
+            return place_tile.data[location].getColor();
+        }
+        return default_color;
+    };
 
     struct SquareDef {
         pub dx: i8,
         pub dy: i8,
         pub id: String,
+        pub color: Color32,
     }
     let square_defns = [
         SquareDef {
             dx: 0,
             dy: 0,
             id: "center".to_owned(),
+            color: get_color(2),
         },
         SquareDef {
             dx: -1,
             dy: 0,
             id: "left".to_owned(),
+            color: get_color(1),
         },
         SquareDef {
             dx: 1,
             dy: 0,
             id: "right".to_owned(),
+            color: get_color(3),
         },
         SquareDef {
             dx: 0,
             dy: -1,
             id: "top".to_owned(),
+            color: get_color(0),
         },
         SquareDef {
             dx: 0,
             dy: 1,
             id: "bottom".to_owned(),
+            color: get_color(4),
         },
     ];
 
@@ -86,7 +97,7 @@ fn tile_ui(ui: &mut egui::Ui, size: f32, on: &mut bool) -> egui::Response {
             ),
             vec2(mini_size, mini_size),
         );
-        let mini_response = rect_button(ui, mini_rect, ui.id().with(def.id.as_str()));
+        let mini_response = rect_button(ui, mini_rect, ui.id().with(def.id.as_str()), def.color);
         if mini_response.clicked() {
             response.ctx.data_mut(|map| {
                 let id = Id::new(SUBTILE_ID);
@@ -98,19 +109,18 @@ fn tile_ui(ui: &mut egui::Ui, size: f32, on: &mut bool) -> egui::Response {
     return response;
 }
 
-fn rect_button(ui: &mut egui::Ui, rect: Rect, id: Id) -> egui::Response {
+fn rect_button(ui: &mut egui::Ui, rect: Rect, id: Id, color: Color32) -> egui::Response {
     let response = ui.interact(rect, id, egui::Sense::click());
     let visuals = ui.style().interact(&response);
     if ui.is_rect_visible(rect) {
         let rect = rect.expand(visuals.expansion);
-        ui.painter()
-            .rect(rect, 0.0, visuals.bg_fill, visuals.bg_stroke);
+        ui.painter().rect(rect, 0.0, color, visuals.bg_stroke);
     }
     response
 }
 
-fn tile(size: f32, on: &mut bool) -> impl egui::Widget + '_ {
-    move |ui: &mut egui::Ui| tile_ui(ui, size, on)
+fn tile(size: f32, tile: &Option<PlacedTile>) -> impl egui::Widget + '_ {
+    move |ui: &mut egui::Ui| tile_ui(ui, size, tile)
 }
 
 impl eframe::App for MyApp {
@@ -143,7 +153,9 @@ impl eframe::App for MyApp {
                             for c in 0..grid_cols {
                                 let mut on = false;
                                 let response = ui
-                                    .push_id((r, c), |ui| ui.add(tile(self.zoom as f32, &mut on)))
+                                    .push_id((r, c), |ui| {
+                                        ui.add(tile(self.zoom as f32, self.board.at(r, c)))
+                                    })
                                     .inner;
                                 response.ctx.data_mut(|map| {
                                     let subtile_id = Id::new(SUBTILE_ID);
