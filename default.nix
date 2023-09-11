ad2f01f776ae19126eadcb92c97c0d66a78ef361
jackschu <js9pa@virginia.edu>
2023-09-11 17:57:20 -0400

    nix build MVP

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> { } }: {
+  out = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "crabcassonne";
+    version = "0.1.0";
+
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+    libPath = with pkgs; lib.makeLibraryPath [
+      libGL
+      libxkbcommon
+      wayland
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXrandr
+    ];
+
+
+
+    buildInputs = with pkgs; [
+      glibc
+      gcc-unwrapped
+    ];
+    postInstall = ''
+            wrapProgram "$out/bin/crabcassonne" --prefix LD_LIBRARY_PATH : "${libPath}"
+            '';
+
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+    meta = { description = "Crabcassonne"; };
+  };
+}
