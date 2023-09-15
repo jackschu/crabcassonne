9d98e7e15ca67b50c05b5110e03f2a96668d8e00
jackschu <js9pa@virginia.edu>
2023-09-15 00:36:17 -0400

    factor out referee state

--- src/referee.rs
+++ src/referee.rs
@@ -1,50 +1,94 @@
-use std::sync::mpsc::{Receiver, Sender};
+use std::{
+    collections::HashMap,
+    sync::mpsc::{Receiver, Sender},
+};
 
 use crate::{
     board::BoardData,
-    board::ConcreteBoard,
+    board::{ConcreteBoard, Coordinate},
     render::{InteractionMessage, RenderMessage},
+    tile::TileData,
     tilebag::TileBag,
 };
 
+pub struct RefereeState {
+    pub tilebag: TileBag,
+    pub board: ConcreteBoard,
+    turn_order: Vec<Player>,
+    turn_idx: usize,
+    player_scores: HashMap<Player, u32>,
+}
+
+impl Default for RefereeState {
+    fn default() -> Self {
+        RefereeState {
+            board: ConcreteBoard::default(),
+            tilebag: TileBag::default(),
+            turn_order: vec![Player::White, Player::Black],
+            turn_idx: 0,
+            player_scores: HashMap::from([(Player::White, 0), (Player::Black, 0)]),
+        }
+    }
+}
+
+impl RefereeState {
+    pub fn progress_turn(&mut self) {
+        self.turn_idx = (self.turn_idx + 1) % self.turn_order.len();
+    }
+    pub fn is_legal_placement(&self, coord: Coordinate, tile: &TileData) -> bool {
+        if self.board.tiles_placed() == 0 {
+            return true;
+        }
+        let legal_tiles = self.board.get_legal_tiles();
+        if !legal_tiles.contains(&coord) {
+            return false;
+        }
+
+        if !self.board.is_features_match(&coord, &tile) {
+            return false;
+        }
+        return true;
+    }
+}
+
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+pub enum Player {
+    Black,
+    White,
+}
+
 pub fn referee_main(receiver: Receiver<InteractionMessage>, sender: Sender<RenderMessage>) {
-    let mut board = ConcreteBoard::default();
-    let mut tilebag = TileBag::default();
-    if let Some(tile) = tilebag.peek() {
+    let mut state = RefereeState::default();
+    if let Some(tile) = state.tilebag.peek() {
         sender
             .send(RenderMessage::PreviewTile(tile.clone()))
             .unwrap();
     }
     loop {
-        sender.send(RenderMessage::NewBoard(board.clone())).unwrap();
+        sender
+            .send(RenderMessage::NewBoard(state.board.clone()))
+            .unwrap();
         match receiver.recv().unwrap() {
             InteractionMessage::Print(message) => {
                 println!("recv {}", message);
             }
             InteractionMessage::Click(message) => {
-                if board.tiles_placed() != 0 {
-                    let legal_tiles = board.get_legal_tiles();
-                    if !legal_tiles.contains(&(message.coord)) {
-                        continue;
+                let maybe_next = state.tilebag.peek();
+
+                if let Some(next) = maybe_next {
+                    let mut next = next.clone();
+                    next.rotation = message.rotation.clone();
+                    if state.is_legal_placement(message.coord, &next) {
+                        state.tilebag.pull();
+                        let points = state.board.get_completion_points(&message.coord, &next);
+                        println!("scored points {}", points);
+                        state.board.set(message.coord, next);
+                        state.progress_turn();
                     }
-                    let maybe_next = tilebag.peek();
-                    if let Some(next) = maybe_next {
-                        let mut next = next.clone();
-                        next.rotation = message.rotation.clone();
-                        if !board.is_features_match(&(message.coord), &next) {
-                            continue;
-                        }
-                    }
-                }
-                if let Some(mut tile) = tilebag.pull() {
-                    tile.rotation = message.rotation;
-                    let points = board.get_completion_points(&message.coord, &tile);
-                    println!("scored points {}", points);
-                    board.set(message.coord, tile);
                 } else {
                     println!("out of tiles");
                 }
-                if let Some(tile) = tilebag.peek() {
+                if let Some(tile) = state.tilebag.peek() {
                     sender
                         .send(RenderMessage::PreviewTile(tile.clone()))
                         .unwrap();
